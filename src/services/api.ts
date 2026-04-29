d11f3857b24ce3b822d0e9b8039b7360f0a13463
annisazaf <annisazhafiraad@gmail.com>
2026-04-29 20:24:55 +0700

    fix: upload khs

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 const TOKEN_KEY = "chatbotfik_token";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
-// 🔥 AUTO KIRIM TOKEN KE SEMUA REQUEST
+// AUTO KIRIM TOKEN KE SEMUA REQUEST
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem(TOKEN_KEY);
 
